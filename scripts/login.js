b45be784651e68df326a8f6a52f0fe52d3240fc2
tharpjan <209424158+tharpjan@users.noreply.github.com>
2025-11-25 19:58:48 +0800

    Update login.js

--- scripts/login.js
+++ scripts/login.js
@@ -121,7 +121,7 @@ async function main() {
       await notifyTelegram({ ok: true, stage: '进入服务器页面', msg: '已成功打开服务器详情', screenshotPath: spServer });
 
       // **点击 Console 菜单**
-      const consoleMenu = page.locator('a[href="/server/71178ed1"].active');
+      const consoleMenu = page.locator('a[href="/server/ef9f8a26"].active');
       await consoleMenu.waitFor({ state: 'visible', timeout: 15_000 });
       await consoleMenu.click({ timeout: 5_000 });
 
